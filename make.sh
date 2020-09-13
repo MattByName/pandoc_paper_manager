4cd319acf508fabdd6224a5102fbec4823400f18
Matt Barnard <matt@serverus.co.uk>
2020-09-13 12:31:18 +0000

    upgraded to include word doc, reference doc, and force revealjs source

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source project.config
+source ./project.config
 
 # Clear PDFs
 
@@ -7,13 +7,15 @@ rm -f $NORMAL_FILE
 rm -f $PERFORMANCE_FILE
 rm -f $SLIDES_REVEAL_FILE
 rm -f $SLIDES_PDF_FILE
-
-
+rm -f $DOC_FILE
+echo $REFERENCE_DOC
 mkdir -p $EXPORT_FOLDER
 
 # Make Normal
 pandoc $SRC_FILE --pdf-engine=xelatex -V papersize:a4 -o $NORMAL_FILE
+pandoc $SRC_FILE --reference-doc $REFERENCE_DOC -V papersize:a4 -o $DOC_FILE
 cp -f $NORMAL_FILE $EXPORT_FOLDER
+cp -f $DOC_FILE $EXPORT_FOLDER
 # Make Performance
 pandoc $SRC_FILE --pdf-engine=xelatex -V papersize:a4 -V fontsize=20pt -V geometry:"top=2.5cm, bottom=2.5cm, left=2.5cm, right=2.5cm" -o $PERFORMANCE_FILE
 
@@ -24,11 +26,13 @@ pandoc -t beamer -V theme=PaloAlto -V colortheme=albatross -V aspectratio=169 -s
 
 cp -f $SLIDES_PDF_FILE $EXPORT_FOLDER
 # Make Slides js
-pandoc -t revealjs -V theme=night-matt -V margin-0.1 -s $SRC_FILE_SLIDES -o $SLIDES_REVEAL_FILE
+pandoc -t revealjs -V revealjs-url=./reveal.js -V theme=beige-matt -V margin-0.1 -s $SRC_FILE_SLIDES -o $SLIDES_REVEAL_FILE
 
 cp -f $SLIDES_REVEAL_FILE $EXPORT_FOLDER
 
 #V margin=0 -V minScale=1 -V maxScale=1 -V  
+
+
 
 
 
